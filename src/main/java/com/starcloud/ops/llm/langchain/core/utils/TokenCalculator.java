83d907a75d2a6805c13390599f05713443291d04
df007df <duanfeisj@gmail.com>
2023-10-01 18:07:39 +0800

    fix

--- src/main/java/com/starcloud/ops/llm/langchain/core/utils/TokenCalculator.java
+++ src/main/java/com/starcloud/ops/llm/langchain/core/utils/TokenCalculator.java
@@ -1,6 +1,5 @@
 package com.starcloud.ops.llm.langchain.core.utils;
 
-import com.knuddels.jtokkit.api.ModelType;
 import com.starcloud.ops.llm.langchain.core.schema.ModelTypeEnum;
 
 import java.math.BigDecimal;

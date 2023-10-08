dd881b88e399ccfe0b16a57aa536116475033100
df007df <duanfeisj@gmail.com>
2023-10-08 13:53:42 +0800

    fix

--- src/main/java/com/starcloud/ops/llm/langchain/core/model/chat/ChatOpenAI.java
+++ src/main/java/com/starcloud/ops/llm/langchain/core/model/chat/ChatOpenAI.java
@@ -154,7 +154,7 @@ public class ChatOpenAI extends BaseChatModel<ChatCompletionResult> {
                             String resultMsg = sb.toString();
 
                             //把已经返回的内容正常记录
-                            if (chatResult.getUsage() != null && chatResult.getUsage().getTotalTokens() == null) {
+                            if (chatResult.getUsage() != null && StrUtil.isNotBlank(resultMsg)) {
 
                                 Long resultToke = this.getNumTokens(resultMsg);
                                 Long totalTokens = resultToke + requestToken;
